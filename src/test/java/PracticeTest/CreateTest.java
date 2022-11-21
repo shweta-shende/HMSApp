6894c3d02376db3b7b4d9f0e817092399f2bf34b
Shweta shende <shwetashende18@gnail.com>
2022-11-21 15:56:22 +0530

    created program

---
+++ src/test/java/PracticeTest/CreateTest.java
@@ -0,0 +1,11 @@
+package PracticeTest;
+
+public class CreateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new Project import");
+
+	}
+
+}
